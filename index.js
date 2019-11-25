ed4fa0d9776a34777c710453435363801aa38005
DanielKRing1 <DanielKRing1@gmail.com>
2019-11-25 15:15:36 -0800

    App entry point, starts email and express servers

---
+++ index.js
@@ -0,0 +1,9 @@
+// Currently only have one server machine, so
+// Run this file for a simple way to run api/email/etc from one entry point
+// In future, could split api/emai/etc processes onto separate servers
+
+require('./email/server');
+require('./api/server');
+
+// Require database server?
+// ...
