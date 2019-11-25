d90e40510321329d720255d6501a7f6942a7186f
DanielKRing1 <DanielKRing1@gmail.com>
2019-11-24 19:29:42 -0800

    Mailcow too large for server, so 1st attempt with 'smtp-server'

---
+++ server.js
@@ -0,0 +1,34 @@
+// smtp.js
+const {SMTPServer} = require('smtp-server');
+
+const server = new SMTPServer({
+    // disable STARTTLS to allow authentication in clear text mode
+    disabledCommands: ['STARTTLS', 'AUTH'],
+    logger: true,
+    onData(stream, session, callback){
+        stream.pipe(process.stdout); // print message to console
+        stream.on('end', callback);
+    },
+});
+
+server.listen(25);
+
+
+// const SMTPServer = require("smtp-server").SMTPServer;
+
+// const options = {
+//     onData(stream, session, callback) {
+//         stream.pipe(process.stdout); // print message to console
+//         stream.on("end", callback);    }
+// };
+// const server = new SMTPServer(options);
+
+// const PORT = 3000 || process.env.PORT;
+// server.listen(PORT, () => console.log(`Running email server on port ${PORT}!`));
+
+// server.on('error', err => {
+//     console.log(`Error: ${err.message}`);
+// });
+// function callback(data) {
+//     console.log(`On data: ${data}`);
+// }
