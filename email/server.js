3eff2ef9bb64f7c89ea049bfc3fe922f8e60a0da
Daniel  Pan <danielpan@daniels-mbp.home>
2019-12-11 22:23:28 -0800

    Added transport

--- email/server.js
+++ email/server.js
@@ -1,19 +1,45 @@
-// smtp.js
-const {SMTPServer} = require('smtp-server');
+const { SMTPServer } = require('smtp-server');
+// const SMTPConnection = require('nodemailer/lib/smtp-connection');
 
 const server = new SMTPServer({
-    // disable STARTTLS to allow authentication in clear text mode
-    disabledCommands: ['STARTTLS', 'AUTH'],
-    logger: true,
-    onData(stream, session, callback){
-        stream.pipe(process.stdout); // print message to console
-        stream.on('end', callback);
-    },
+  // disable STARTTLS to allow authentication in clear text mode
+  disabledCommands: ['STARTTLS', 'AUTH'],
+  logger: true,
+  onConnect(session, callback) {
+    console.log('woah');
+    console.log('session.remoteAddress:', session.remoteAddress);
+    // if (session.remoteAddress === '127.0.0.1') {
+    //   return callback(new Error('No connections from localhost allowed'));
+    // }
+    return callback(); // Accept the connection
+  },
+  onData(stream, session, callback) {
+    stream.pipe(process.stdout); // print message to console
+    stream.on('end', callback);
+  },
+});
+
+server.on('error', (err) => {
+  console.log('Error occurred');
+  console.log(err);
 });
 
 const PORT = process.env.EMAIL_PORT || 3006;
 server.listen(PORT, () => console.log(`Running email server on port ${PORT}!`));
 
+// const connection = new SMTPConnection({
+//   port: 3006,
+//   logger: true,
+// });
+
+// connection.connect(() => console.log('connected!'));
+
+// const envelope = { from: 'daniel@gmail.com', to: 'dring@gmail.com' };
+// const message = 'test';
+
+// sudo node ./email/server.js &sendmail test@localhost < email.txt
+
+// connection.send(envelope, message, (err) => console.log(err));
 
 // const SMTPServer = require("smtp-server").SMTPServer;
 
---
+++ email/transport.js
@@ -0,0 +1,42 @@
+const nodemailer = require('nodemailer');
+
+const smtpTransport = nodemailer.createTransport({
+  port: 3006,
+  host: '52.52.199.187',
+});
+
+smtpTransport.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Server is ready to take our messages');
+  }
+});
+
+// Message
+// const message = {
+//   from: 'me@localhost.com',
+//   replyTo: 'me@localhost.com',
+//   to: 'me@localhost',
+//   subject: 'hello',
+// };
+const message = {
+  from: 'sender@server.com',
+  to: 'receiver@sender.com',
+  subject: 'Message title',
+  text: 'Plaintext version of the message',
+  html: '<p>HTML version of the message</p>',
+};
+
+console.log('Sending Mail');
+// Send mail
+smtpTransport.sendMail(message, (error, info) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Message sent successfully!');
+    console.log('Server responded with "%s"', info.response);
+  }
+  console.log('Closing Transport');
+  smtpTransport.close();
+});
